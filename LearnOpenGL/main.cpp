4809c336f6e47dc7f53190ce8760cd3eaae0cb31
penglei <2643650181@qq.com>
2020-04-18 22:27:19 +0800

    # version 0.2

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -1,9 +1,57 @@
 #include <glad/glad.h> // 必须放在 "#include <GLFW\glfw3.h>" 之前
-#include <GLFW\glfw3.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
 
-int main() {
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
 
+void FramebufferSizeCallback(GLFWwindow *window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
 
+void ProcessInput(GLFWwindow *window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
+int main(int argc, char **argv) {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Mac OS X系统可能需要
+
+	GLFWwindow *window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "LearnOpenGL", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window." << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD." << std::endl;
+		return -1;
+	}
+
+	glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
+
+	while (!glfwWindowShouldClose(window)) {
+		// 输入
+		ProcessInput(window);
+
+		// 渲染
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// 检查并调用事件，交换缓冲
+		glfwPollEvents();
+		glfwSwapBuffers(window);		
+	}
+
+	glfwTerminate();
 	return 0;
 }
 
--- RELEASE.md
+++ RELEASE.md
@@ -1,3 +1,8 @@
+# version 0.2
+
+* 绘制指定颜色背景窗口
+* [你好，窗口](https://learnopengl-cn.github.io/01%20Getting%20started/03%20Hello%20Window/) 
+
 # version 0.1
 
 * 初始化项目，配置 GLFW 和 GLAD
