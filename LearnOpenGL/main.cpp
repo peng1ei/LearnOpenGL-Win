622d0a697752613fc248b53dd84bfb27dfa1c41b
penglei <2643650181@qq.com>
2020-04-24 07:20:06 +0800

    Add doc

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -1,13 +1,21 @@
 // mogl -- My OpenGL
+#include "CommonDefine.h"
 #include "01_Getting_Started/02_Hello_Triangle/HelloTriangle.h"
 #include "01_Getting_Started/02_Hello_Triangle/HelloTriangleIndexed.h"
 #include "01_Getting_Started/02_Hello_Triangle/Exercise/Ex1_TwoTriangles.h"
 #include "01_Getting_Started/02_Hello_Triangle/Exercise/Ex2_TwoTriangles2.h"
 #include "01_Getting_Started/02_Hello_Triangle/Exercise/Ex3_TwoTriangles3.h"
 
-
+void GetOpenGLInformation() {
+	int nrAttributes;
+	glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
+	std::cout << "Maximum nr of vertex attributes supported: " << nrAttributes << std::endl;
+}
 
 int main(int argc, char **argv) {
+
+	
+
 	int ret = 0;
 
 	//ret = mogl::ch121::HelloTriangle::Run(argc, argv);
@@ -15,7 +23,7 @@ int main(int argc, char **argv) {
 	//ret = mogl::ch12ex1::TwoTriangles::Run(argc, argv);
 	//ret = mogl::ch12ex2::TwoTriangles2::Run(argc, argv);
 	ret = mogl::ch12ex3::TwoTriangles3::Run(argc, argv);
-	
+
 	return ret;
 }
 
